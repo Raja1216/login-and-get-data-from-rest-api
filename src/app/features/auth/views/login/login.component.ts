d8df3bde383d5dd97e450c76f7cb6666481a20f8
Subhajit Paramanik <opz-shb@localhost.localdomain>
2022-08-08 11:23:00 +0530

    Code commited for log remove

--- src/app/features/auth/views/login/login.component.ts
+++ src/app/features/auth/views/login/login.component.ts
@@ -6,76 +6,84 @@ import { LoginService } from '../../services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm = this._fb.group({
     user_id: ['', [Validators.required]],
     password: ['', Validators.required],
   });
-  error:string='';
-  userarray:object | any =[];
+  error: string = '';
+  userarray: object | any = [];
   loginUserDetails: object | any;
-  constructor( private _fb: FormBuilder,private _auth:LoginService,private _router: Router) { }
+  constructor(
+    private _fb: FormBuilder,
+    private _auth: LoginService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
-   this.getUsers();
+    // this.getUsers();
   }
-  attemptLogin(){
-    if (this.loginForm.value.user_id == null || this.loginForm.value.user_id =='' ) {
+  attemptLogin() {
+    if (
+      this.loginForm.value.user_id == null ||
+      this.loginForm.value.user_id == ''
+    ) {
       this.error = 'Enter user id please.';
       setTimeout(() => {
-        this.error ='';
+        this.error = '';
       }, 1000);
       return;
     }
-    if (this.loginForm.value.password == null || this.loginForm.value.password =='' ) {
+    if (
+      this.loginForm.value.password == null ||
+      this.loginForm.value.password == ''
+    ) {
       this.error = 'Enter password please.';
       setTimeout(() => {
-        this.error ='';
+        this.error = '';
       }, 1000);
       return;
     }
     if (!this.loginForm.valid) {
       this.error = 'The user credentials were incorrect.';
       setTimeout(() => {
-        this.error ='';
+        this.error = '';
       }, 1000);
       return;
     }
-    this._auth.signIn({user_id:this.loginForm.value.user_id,password:this.loginForm.value.password}).subscribe(
-      (res: any) => {
-       console.log(res);
-       this.loginUserDetails = res
-       if (this.loginUserDetails.length >0) {
-        localStorage.setItem('user', this.loginUserDetails[0].name);
-        localStorage.setItem('user_id', this.loginUserDetails[0].id);
-        console.log(this.loginUserDetails);
-         this._router.navigateByUrl('cbt/home');
-       }
-       else
-       {
-         this.error = 'The user credentials were incorrect.';
-        setTimeout(() => {
-          this.error ='';
-        }, 1000);
-        return;
-       }
-      },
-      (err:any) =>
-      {
-        console.log(err);
-      }
-    );
-
+    this._auth
+      .signIn({
+        user_id: this.loginForm.value.user_id,
+        password: this.loginForm.value.password,
+      })
+      .subscribe(
+        (res: any) => {
+          this.loginUserDetails = res;
+          if (this.loginUserDetails.length > 0) {
+            localStorage.setItem('user', this.loginUserDetails[0].name);
+            localStorage.setItem('user_id', this.loginUserDetails[0].id);
+            this._router.navigateByUrl('cbt/home');
+          } else {
+            this.error = 'The user credentials were incorrect.';
+            setTimeout(() => {
+              this.error = '';
+            }, 1000);
+            return;
+          }
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
   }
-  getUsers(){
+  getUsers() {
     this._auth.getUsers().subscribe(
       (res: any) => {
-       console.log(res);
+        console.log(res);
       },
-      (err:any) =>
-      {
+      (err: any) => {
         console.log(err);
       }
     );
--- src/app/features/employee-details/employee-details.component.ts
+++ src/app/features/employee-details/employee-details.component.ts
@@ -5,36 +5,36 @@ import { HomeService } from '../home/services/home.service';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.scss']
+  styleUrls: ['./employee-details.component.scss'],
 })
 export class EmployeeDetailsComponent implements OnInit {
-  empId:number;
-  employeeDetails:object | any
-  isLoding:boolean = false
-  constructor(private _activatedRoute: ActivatedRoute,private _homeService:HomeService) {
+  empId: number;
+  employeeDetails: object | any;
+  isLoding: boolean = false;
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _homeService: HomeService
+  ) {
     this.empId = this._activatedRoute.snapshot.params['id'];
   }
 
   ngOnInit(): void {
     this.getEmpDetails(this.empId);
   }
-  getEmpDetails(empId:number){
-    this.isLoding= true;
-    this._homeService.getEmployeeDetails({id:empId}).subscribe(
+  getEmpDetails(empId: number) {
+    this.isLoding = true;
+    this._homeService.getEmployeeDetails({ id: empId }).subscribe(
       (res: any) => {
-        this.isLoding= false;
-       this.employeeDetails = res[0]
-       console.log(this.employeeDetails);
-
+        this.isLoding = false;
+        this.employeeDetails = res[0];
       },
-      (err:any) =>
-      {
+      (err: any) => {
         console.log(err);
       }
     );
   }
-  getMoreDetails(){
-    this.employeeDetails.view_more
-    window.open(this.employeeDetails.view_more, "_blank");
+  getMoreDetails() {
+    this.employeeDetails.view_more;
+    window.open(this.employeeDetails.view_more, '_blank');
   }
 }
